a39fd59ea598be00e841f86a4d624c9d772f8c00
domi <meyerd@mytum.de>
2009-08-19 01:11:39 +0200

    - identation error in memory_manager.py

--- memory_manager.py
+++ memory_manager.py
@@ -98,7 +98,7 @@ Members are conveniently accessable:
     def __repr__(self):
 	return "<Memory %s @0x%x>" % (repr(self.__type), self.__loc)
     def memcmp(self):
-	    "compares the symbol in the two mapped memory images, returns true if the symbol didn't change"
+	"compares the symbol in the two mapped memory images, returns true if the symbol didn't change"
         return self.__type.memcmp(self.__loc, self.__loc, 0, {})
     def to_xml(self, depth=MAX_DEPTH):
 	return to_xml(self.__value(depth)).toprettyxml(indent="  ")
